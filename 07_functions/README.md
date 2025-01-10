f545c8b69dec07c0031c384a7b1a381c88e2a95f
Reece Hunter <heecerunter@gmail.com>
2025-01-10 15:42:30 -0800

    Finished 07_functions

--- 07_functions/README.md
+++ 07_functions/README.md
@@ -1,6 +1,11 @@
 # Task
 Write functions that meet the following criteria:
-1. Takes in parameters "width" and "height" and returns the area of a rectangle.
-2. Takes in parameters 
+1. Takes two integers as parameters and returns the sum.
+2. Takes in an array as a parameter and prints each value of it. 
 
 # Example output
+```
+Sum: 15.
+
+Array values: 1 2 3 4 5
+```
--- 07_functions/main.c
+++ 07_functions/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
+// Adds two integers and returns the result.
 int sum(int x, int y) {
 	int sum = x + y;
 	return sum;
 }
 
-
+// Prints each integer in an array.
+void display(int* nums, size_t length) {
+	for(size_t i = 0; i < length; i++) {
+		int num = nums[i];
+		printf("%d ", num);
+	}
+	printf("\n");
+}
 
 int main() {
+	// Initialize integers and pass them to the sum function.
 	int a = 5, b = 10;
 	int a_plus_b = sum(a, b);
 	printf("Sum of %d + %d = %d.\n", a, b, a_plus_b);
+
+	// Initialize an array of integers and pass them to the display function.
+	int nums[5] = {1, 2, 3, 4, 5};
+	printf("Array values: ");
+	display(nums, 5);
 
 	return 0;
 }
